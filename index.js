73dd1dffbc6060632898f8887eec14d3176f4bae
wildkat246 <85767537+wildkat246@users.noreply.github.com>
2021-09-06 20:55:28 -0400

    1st push

---
+++ index.js
@@ -0,0 +1,24 @@
+const render = require("./src/page-template.js");
+const team = [
+    {
+        name: "Blake",
+        id: 1,
+        email: "begordon22@gmail.com",
+        role: "manager"
+    },
+    {
+        name: "Randy",
+        id: 2,
+        email: "randy@gmail.com",
+        role: "engineer"
+    },
+    {
+        name: "Mike",
+        id: 3,
+        email: "mike@gmail.com",
+        role: "intern"
+    }
+];
+console.log(render(team));
+
+
